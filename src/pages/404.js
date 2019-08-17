595c788e6cd47666af3cc5e790a702849aca865f
ryo4004 <regs.ryo4004@gmail.com>
2019-08-17 12:32:34 +0900

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -13,9 +13,9 @@ class NotFoundPage extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
+        <SEO title="404 Not Found" />
         <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <p><Link to='/'>Home</Link></p>
       </Layout>
     )
   }
