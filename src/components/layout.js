a95692b6f54fa48b1358041a296c38ea0f737770
ryo4004 <regs.ryo4004@gmail.com>
2019-08-17 11:33:56 +0900

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import { rhythm, scale } from "../utils/typography"
-
 import './reset.scss'
 import './layout.scss'
 
@@ -14,57 +12,21 @@ class Layout extends React.Component {
 
     if (location.pathname === rootPath) {
       header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
+        <header className='header-index'>
+          <h1><Link to={`/`}>{title}</Link></h1>
+        </header>
       )
     } else {
       header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
+        <header className='header-blog-post'>
+          <h3><Link to={`/`}>{title}</Link></h3>
+        </header>
       )
     }
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
+      <div className='layout'>
         <header>{header}</header>
-        <main>{children}</main>
+        <main className='contents'>{children}</main>
         <footer>
           &copy; {new Date().getFullYear()} Akane
         </footer>
