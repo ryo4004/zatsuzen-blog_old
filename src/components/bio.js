aa8f31d73ef7ea4468767ad9324b4c2b534682c0
ryo4004 <regs.ryo4004@gmail.com>
2019-08-17 11:17:28 +0900

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -34,28 +34,11 @@ const Bio = () => {
 
   const { author, social } = data.site.siteMetadata
   return (
-    <div
-      style={{
-        display: `flex`,
-        marginBottom: rhythm(2.5),
-      }}
-    >
-      <Image
-        fixed={data.avatar.childImageSharp.fixed}
-        alt={author}
-        style={{
-          marginRight: rhythm(1 / 2),
-          marginBottom: 0,
-          minWidth: 50,
-          borderRadius: `100%`,
-        }}
-        imgStyle={{
-          borderRadius: `50%`,
-        }}
-      />
-      <p>
-        Written by Akane
-      </p>
+    <div className='bio'>
+      <div>
+        <Image fixed={data.avatar.childImageSharp.fixed} alt={author} />
+        <p>Written by Akane</p>
+      </div>
     </div>
   )
 }
