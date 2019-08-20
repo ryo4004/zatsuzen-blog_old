c04ff8b58f858483a7ca2d5c67689a55e0036571
ryo4004 <regs.ryo4004@gmail.com>
2019-08-20 15:11:30 +0900

    Add gatsby-plugin-catch-links

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,7 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    'gatsby-plugin-catch-links'
   ],
 }
